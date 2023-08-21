5ad5bbbe5a71d7613743b7f155d365158120d6b7
JustCheka <cheka201@yandxe.ru>
2023-08-22 00:34:18 +0300

    First ex

---
+++ ex1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число");
+int numA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int numB = int.Parse(Console.ReadLine());
+
+if (numA > numB){
+    Console.WriteLine("Число А больше числа Б");
+}
+else if (numB > numA){
+    Console.WriteLine("Число Б больше числа А");
+}
+else Console.WriteLine("Числа равны");
