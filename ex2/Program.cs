e45b6d43fe4cc1a4dd24b1e3a8b9535b62fa9323
JustCheka <cheka201@yandxe.ru>
2023-08-22 00:41:25 +0300

    Ex 2

---
+++ ex2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+int c = int.Parse(Console.ReadLine());
+
+if (a > b && a > c) Console.WriteLine($"Самое большое число = {a}");
+else if (b > a && b > c) Console.WriteLine($"Самое большое число = {b}");
+else if (c > a && c > b) Console.WriteLine($"Самое большое число = {c}");
